6b18b2e8840add08c3656de3ae9fde44e30abbd6
ql143 <ql143@duke.edu>
2021-09-08 15:12:13 -0400

    commit

--- 028_tests_power/test-power.c
+++ 028_tests_power/test-power.c
@@ -6,11 +6,11 @@ unsigned power(unsigned x, unsigned y);
 void run_check(unsigned x, unsigned y, unsigned expeted_ans);
 
 int main(void) {
-  run_check(2, 2, 3);
   //check incorrect input like the negative one (maybe it is a bad idear)
 
   //check for correct input----------
   //check if base is 0
+
   run_check(0, 0, 1);
   run_check(0, 1, 0);
   run_check(0, 4294967295, 0);
@@ -30,7 +30,7 @@ int main(void) {
   run_check(1, 1, 1);
   run_check(4294967294, 1, 4294967294);
   run_check(2147483646, 1, 2147483646);
-  run_check(-1, 1, -1);
+
   run_check(2147483647, 1, 2147483647);
   //checking overflow if first is int
   run_check(2147483647, 1, 2147483647);
@@ -47,23 +47,17 @@ int main(void) {
 
   //run some overflow
 
-  //check
-  run_check(-1, 0, 1);
-  run_check(-2147483648, 0, 1);
-  run_check(-2147483648, 1, -2147483648);
-  run_check(-1, -1, -1);
-  run_check(-1, 2, 1);
   //run_check(4294967296, 0, 1);
   //run_check(4294967295, 2, 18446744065119617000);
   //run_check(4294967296, 1, 4294967296);
-  run_check(2, 2, 4);
+
   return EXIT_SUCCESS;
 }
 
 void run_check(unsigned x, unsigned y, unsigned expected_ans) {
   unsigned ans = power(x, y);
   if (ans == expected_ans) {
-    exit(EXIT_SUCCESS);
+    //exit(EXIT_SUCCESS);
   }
   else {
     printf(
