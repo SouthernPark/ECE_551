d5ebc0cf1a24cc939a048297d063a9b7e4cb89c1
ql143 <ql143@duke.edu>
2021-09-28 16:17:27 -0400

    commit

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,24 +14,7 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
-int main(int argc, char ** argv) {
-  //WRITE YOUR CODE HERE!
-  FILE * f = NULL;
-  if (argc == 1) {
-    f = stdin;
-    if (f == NULL) {
-      fprintf(stderr, "Can not read from stdin");
-      return EXIT_FAILURE;
-    }
-  }
-  else {
-    f = fopen(argv[1], "r");
-    if (f == NULL) {
-      perror("can not open the file:");
-      return EXIT_FAILURE;
-    }
-  }
-
+void readSortPrint(FILE * f) {
   char ** lines = NULL;
   size_t i = 0;
   char * line = NULL;
@@ -42,15 +25,13 @@ int main(int argc, char ** argv) {
     char ** tmp = realloc(lines, (i + 1) * sizeof(*tmp));
     if (tmp == NULL) {
       fprintf(stderr, "run out of memo");
-      return EXIT_FAILURE;
+      exit(EXIT_FAILURE);
     }
     lines = tmp;
     lines[i] = strdup(line);
     i++;
   }
   free(line);
-  //close the file
-  fclose(f);
 
   //sort the data
   sortData(lines, i);
@@ -59,8 +40,39 @@ int main(int argc, char ** argv) {
     printf("%s", lines[j]);
     free(lines[j]);
   }
-
   free(lines);
+}
+
+int main(int argc, char ** argv) {
+  //WRITE YOUR CODE HERE!
+  FILE * f = NULL;
+  if (argc == 0) {
+    fprintf(stderr, "Argument can not be 0");
+  }
+  else if (argc == 1) {
+    f = stdin;
+    if (f == NULL) {
+      fprintf(stderr, "Can not read from stdin");
+      return EXIT_FAILURE;
+    }
+    readSortPrint(f);
+  }
+  else {
+    for (int i = 1; i < argc; i++) {
+      f = fopen(argv[i], "r");
+      if (f == NULL) {
+        perror("can not open the file:");
+        return EXIT_FAILURE;
+      }
+      readSortPrint(f);
+    }
+
+    //close the file
+    int c = fclose(f);
+    if (c == EOF) {
+      perror("can not close");
+    }
+  }
 
   return EXIT_SUCCESS;
 }
