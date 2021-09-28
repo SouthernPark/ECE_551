3957e5a05371f3af3aacc240a1b20b649459c4cf
ql143 <ql143@duke.edu>
2021-09-28 15:58:09 -0400

    commit

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -16,6 +16,51 @@ void sortData(char ** data, size_t count) {
 
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
+  FILE * f = NULL;
+  if (argc == 1) {
+    f = stdin;
+    if (f == NULL) {
+      fprintf(stderr, "Can not read from stdin");
+      return EXIT_FAILURE;
+    }
+  }
+  else {
+    f = fopen(argv[1], "r");
+    if (f == NULL) {
+      perror("can not open the file:");
+      return EXIT_FAILURE;
+    }
+  }
+
+  char ** lines = NULL;
+  size_t i = 0;
+  char * line = NULL;
+  size_t sz = 0;
+
+  while (getline(&line, &sz, f) != -1) {
+    //store the line to lines
+    char ** tmp = realloc(lines, (i + 1) * sizeof(*tmp));
+    if (tmp == NULL) {
+      fprintf(stderr, "run out of memo");
+      return EXIT_FAILURE;
+    }
+    lines = tmp;
+    lines[i] = strdup(line);
+    i++;
+  }
+  free(line);
+  //close the file
+  fclose(f);
+
+  //sort the data
+  sortData(lines, i);
+  //printz
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+
+  free(lines);
 
   return EXIT_SUCCESS;
 }
