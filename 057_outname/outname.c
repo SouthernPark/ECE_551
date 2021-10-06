6759504f769aa67d80951ee5fbd8463adb591ed5
ql143 <ql143@duke.edu>
2021-10-06 12:16:26 -0400

    commit

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,24 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  size_t len = strlen(inputName);
+
+  char * out = malloc((len + 8) * sizeof(*out));
+  size_t i = 0;
+  for (; i < len; i++) {
+    out[i] = inputName[i];
+  }
+
+  char * postfix = ".counts";
+  for (int j = 0; j < 8; j++) {
+    out[i] = postfix[j];
+    i++;
+  }
+
+  return out;
 }
