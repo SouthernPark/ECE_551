e95f38f5dc9c65a733a06c5ed776563514994781
ql143 <ql143@duke.edu>
2021-10-11 16:06:12 -0400

    commit

--- 064_vector_2d/vector.cpp
+++ 064_vector_2d/vector.cpp
@@ -2,6 +2,23 @@
 
 #include <cmath>
 #include <cstdio>
-
+using std::sqrt;
 /* write your class implementation in this file
  */
+
+double Vector2D ::getMagnitude() const {
+  return sqrt(x * x + y * y);
+}
+
+Vector2D Vector2D::operator+(const Vector2D & rhs) const {
+  Vector2D res;
+  res.initVector(x + rhs.x, y + rhs.y);
+
+  return res;
+}
+
+Vector2D & Vector2D::operator+=(const Vector2D & rhs) {
+  x += rhs.x;
+  y += rhs.y;
+  return *this;
+}
