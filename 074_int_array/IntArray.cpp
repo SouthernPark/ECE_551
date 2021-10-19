fdd4c37106b1b1654237ad8b3a9830271c712368
ql143 <ql143@duke.edu>
2021-10-19 17:11:25 -0400

    commit

--- 074_int_array/IntArray.cpp
+++ 074_int_array/IntArray.cpp
@@ -74,9 +74,11 @@ bool IntArray::operator!=(const IntArray & rhs) const {
 std::ostream & operator<<(std::ostream & s, const IntArray & rhs) {
   s << "{";
   for (int i = 0; i < rhs.size(); i++) {
-    s << rhs[i] << ", ";
     if (i == rhs.size() - 1) {
       s << rhs[i];
+    }
+    else {
+      s << rhs[i] << ", ";
     }
   }
   s << "}";
