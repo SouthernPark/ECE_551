ba22e7f5cea6dbb578724ce5fdff0c116f094998
ql143 <ql143@duke.edu>
2021-10-19 16:54:16 -0400

    commit

--- 074_int_array/IntArray.cpp
+++ 074_int_array/IntArray.cpp
@@ -4,25 +4,22 @@
 
 #include <ostream>
 
-IntArray::IntArray() {
-  data = NULL;
-  numElements = 0;
+IntArray::IntArray() : data(NULL), numElements(0) {
 }
-IntArray::IntArray(int n) {
-  data = new int[n];
-  numElements = n;
+IntArray::IntArray(int n) : data(new int[n]), numElements(n) {
 }
 
 //deep copy the array specified in rhs
 //used for the this does not eixst
-IntArray::IntArray(const IntArray & rhs) {
+IntArray::IntArray(const IntArray & rhs) :
+    data(new int[rhs.numElements]),
+    numElements(rhs.numElements) {
   //remove the original data
   //delete[] data;
-  data = new int[rhs.numElements];
-  for (int i = 0; i < rhs.numElements; i++) {
+
+  for (int i = 0; i < numElements; i++) {
     data[i] = rhs.data[i];
   }
-  numElements = rhs.numElements;
 }
 IntArray::~IntArray() {
   delete[] data;
@@ -31,20 +28,24 @@ IntArray::~IntArray() {
 //deep assign
 //used for this exist
 IntArray & IntArray::operator=(const IntArray & rhs) {
-  delete[] data;
-  data = new int[rhs.numElements];
-  for (int i = 0; i < rhs.numElements; i++) {
-    data[i] = rhs.data[i];
+  if (this != &rhs) {
+    int * temp = new int[rhs.numElements];
+    for (int i = 0; i < rhs.numElements; i++) {
+      temp[i] = rhs.data[i];
+    }
+    delete[] data;
+    data = temp;
+    numElements = rhs.numElements;
   }
 
   return *this;
 }
 const int & IntArray::operator[](int index) const {
-  assert(index < numElements);
+  assert(index >= 0 && index < numElements);
   return data[index];
 }
 int & IntArray::operator[](int index) {
-  assert(index < numElements);
+  assert(index >= 0 && index < numElements);
   return data[index];
 }
 
@@ -54,10 +55,6 @@ int IntArray::size() const {
 
 bool IntArray::operator==(const IntArray & rhs) const {
   if (numElements != rhs.numElements) {
-    return false;
-  }
-
-  if (numElements == 0) {
     return false;
   }
 
