70f5bfd767ecb37f4a66253466181ab5680da0fe
ql143 <ql143@duke.edu>
2021-11-08 18:47:23 -0500

    commit

--- 096_tests_binsrch/test-bsearch.cpp
+++ 096_tests_binsrch/test-bsearch.cpp
@@ -31,14 +31,14 @@ int main(void) {
   delete sin;
 
   Function<int, int> * negSqure = new NegSquareFunction();  // -(x+1)^2
-  check(negSqure, -1000, 0, -1, "testing for -(x+1)^2 function");
-  check(negSqure, -1000, -1, -2, "testing for -(x+1)^2 function");
+  //check(negSqure, -1000, 0, -1, "testing for -(x+1)^2 function");
+  check(negSqure, -2, -1, -2, "testing for -(x+1)^2 function");
   //all negative
   check(negSqure, -1000, -500, -501, "testing for -(x+1)^2 function");
 
   delete negSqure;
   Function<int, int> * linear = new LinearFunction();  //y = x - 2.5
-  check(linear, 0, 0, 0, "testing for x - 2.5");
+  //check(linear, 0, 0, 0, "testing for x - 2.5");
   check(linear, 0, 1024, 2, "testing for x - 2.5");
   check(linear, 0, 3, 2, "testing for x - 2.5");
   check(linear, 1, 3, 2, "testing for x - 2.5");
@@ -56,4 +56,5 @@ int main(void) {
 
   Function<int, int> * constFunc = new ConstFunction();
   check(constFunc, -1000, 1000, 999, "check for constant");
+  delete constFunc;
 }
