12f95cb56c3609477c5d68e06a528af68971cad7
ql143 <ql143@duke.edu>
2021-11-08 18:55:09 -0500

    commit

--- 097_binsrch/search.cpp
+++ 097_binsrch/search.cpp
@@ -2,8 +2,15 @@
 
 //find the largest elements that are smaller than 0 in [low, high)
 int binarySearchForZero(Function<int, int> * f, int low, int high) {
+  if (low > high) {
+    exit(EXIT_FAILURE);
+  }
+  if (low == high) {
+    return low;
+  }
   int left = low;
   int right = high;
+
   while (left < right) {
     int mid = (right - left + 1) / 2 + left;
     //if f(mid) <= 0, then mid can be the answer
