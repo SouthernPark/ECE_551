51d4fb4767704ffdf41e9d28737dd26becd4e7de
ql143 <ql143@duke.edu>
2021-11-08 18:45:24 -0500

    commit

--- 097_binsrch/function.h
+++ 097_binsrch/function.h
@@ -1,9 +1,72 @@
 #ifndef __FUNCTION_H__
 #define __FUNCTION_H__
-template<typename R, typename A> 
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+template<typename R, typename A>
 class Function {
-public:
+ public:
   virtual R invoke(A arg) = 0;
   virtual ~Function() {}
 };
+
+class CountedIntFn : public Function<int, int> {
+ protected:
+  //record the number of invocations we can do
+  unsigned remaining;
+  Function<int, int> * f;
+  const char * mesg;
+
+ public:
+  //initialization
+  CountedIntFn(unsigned n, Function<int, int> * fn, const char * m) :
+      remaining(n),
+      f(fn),
+      mesg(m) {}
+
+  //
+  virtual int invoke(int arg) {
+    if (remaining == 0) {
+      fprintf(stderr, "Too many function invocations in %s\n", mesg);
+      exit(EXIT_FAILURE);
+    }
+    remaining--;
+    return f->invoke(arg);
+  }
+};
+
+//sin function
+class SinFunction : public Function<int, int> {
+ public:
+  virtual int invoke(int arg) { return 10000000 * (sin(arg / 100000.0) - 0.5); }
+};
+
+//sin function
+class NegSquareFunction : public Function<int, int> {
+ public:
+  virtual int invoke(int arg) { return -(arg + 1) * (arg + 1); }
+};
+
+class LinearFunction : public Function<int, int> {
+ public:
+  virtual int invoke(int arg) { return arg - 2.5; }
+};
+
+class SegFunction : public Function<int, int> {
+ public:
+  virtual int invoke(int arg) {
+    if (arg >= 0) {
+      return arg + 1;
+    }
+    else {
+      return arg - 1;
+    }
+  }
+};
+
+class ConstFunction : public Function<int, int> {
+ public:
+  virtual int invoke(int arg) { return -1; }
+};
+
 #endif
---
+++ 097_binsrch/test-bsearch.cpp
@@ -0,0 +1,64 @@
+#include <assert.h>
+#include <math.h>
+
+#include "function.h"
+
+//binary search
+int binarySearchForZero(Function<int, int> * f, int low, int high);
+
+void check(Function<int, int> * f,
+           int low,
+           int high,
+           int expected_ans,
+           const char * mesg) {
+  //I computed the maximum number of invocations of f
+  int remains = 1;
+  if (high > low) {
+    remains = ((int)log2(high - low)) + 1;
+  }
+  //wrap f in a CountedIntFn,
+  CountedIntFn count(remains, f, mesg);
+  //perform the binary search
+  int res = binarySearchForZero(&count, low, high);
+  //check the answer
+  if (res != expected_ans) {
+    printf("%d\n", res);
+    printf("%s\n", mesg);
+    exit(EXIT_FAILURE);
+  }
+  //assert(res == expected_ans);
+}
+
+int main(void) {
+  Function<int, int> * sin = new SinFunction();
+  check(sin, 0, 150000, 52359, "testing for sin function");
+  check(sin, 52359, 150000, 52359, "testing for sin function");
+  delete sin;
+
+  Function<int, int> * negSqure = new NegSquareFunction();  // -(x+1)^2
+  //check(negSqure, -1000, 0, -1, "testing for -(x+1)^2 function [-1000, 0]");
+  check(negSqure, -2, -1, -2, "testing for -(x+1)^2 function [-2, -1]");
+  //all negative
+  check(negSqure, -1000, -500, -501, "testing for -(x+1)^2 function [-1000, -500]");
+
+  delete negSqure;
+  Function<int, int> * linear = new LinearFunction();  //y = x - 2.5
+  //check(linear, 0, 0, 0, "1.testing for x - 2.5");
+  check(linear, 0, 1024, 2, "2.testing for x - 2.5");
+  check(linear, 0, 3, 2, "3.testing for x - 2.5");
+  check(linear, 1, 3, 2, "4.testing for x - 2.5");
+  check(linear, 2, 3, 2, "5.testing for x - 2.5");
+  check(linear, 1, 2, 1, "6.testing for x - 2.5");
+  //all positive
+  check(linear, 10, 1024, 10, "7.testing for x - 2.5");
+
+  delete linear;
+
+  Function<int, int> * segFunc = new SegFunction();
+  check(segFunc, -1, 1, -1, "8.testing for seg func");
+  check(segFunc, 0, 1, 0, "9.testing for seg func");
+  delete segFunc;
+
+  Function<int, int> * constFunc = new ConstFunction();
+  check(constFunc, -1000, 1000, 999, "check for constant");
+}
