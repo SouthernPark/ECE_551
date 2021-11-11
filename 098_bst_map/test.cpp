304abcee07281d22a1ff9f6478711177ea60dd10
ql143 <ql143@duke.edu>
2021-11-11 13:55:35 -0500

    commit

---
+++ 098_bst_map/test.cpp
@@ -0,0 +1,36 @@
+#include "map.h"
+
+int main(void) {
+  //create a BstMap
+  BstMap<int, int> bmap;
+  //add elements to the bmap
+  bmap.add(6, 6);
+  bmap.add(3, 3);
+  bmap.add(2, 2);
+  bmap.add(1, 1);
+  bmap.add(11, 11);
+  bmap.add(10, 10);
+  bmap.add(13, 13);
+
+  bmap.add(6, 6);
+  bmap.add(3, 3);
+  bmap.add(2, 2);
+  bmap.add(1, 1);
+  bmap.add(11, 11);
+  bmap.add(10, 10);
+  bmap.add(13, 13);
+
+  //bmap.add(2, 3);
+  bmap.print_tree();
+  std::cout << "look up###########\n";
+
+  //look up a key
+  const int x = bmap.lookup(13);
+
+  std::cout << x << std::endl;
+
+  std::cout << "delete a node\n";
+
+  bmap.remove(3);
+  bmap.print_tree();
+}
