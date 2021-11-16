93029b2f7c7837266a2ff25dd358c7ef97e7083d
ql143 <ql143@duke.edu>
2021-11-16 10:16:56 -0500

    commit

--- 104_huff_map/node.cpp
+++ 104_huff_map/node.cpp
@@ -1,5 +1,17 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
   //WRITE ME!
-}
 
+  if (sym != NO_SYM) {
+    assert(left == NULL && right == NULL);
+    //then the bit string represent the symbol
+    theMap.insert(std::pair<unsigned, BitString>(sym, b));
+    return;
+  }
+
+  assert(left != NULL && right != NULL);
+
+  left->buildMap(b.plusZero(), theMap);
+  right->buildMap(b.plusOne(), theMap);
+  return;
+}
